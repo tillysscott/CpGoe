0d4c9cdfb6321370a3166726414ed63a5139b975
tillysscott <125389776+tillysscott@users.noreply.github.com>
2023-07-17 14:46:53 +0100

    Update MeanManualCpG.sh

--- MeanManualCpG.sh
+++ MeanManualCpG.sh
@@ -4,41 +4,41 @@
 #SBATCH -c 2
 #SBATCH --mem 20G
 
-
-head -2 catalog.fa
+export genome="genome"
+head -2 $genome.fa
 echo
 echo number of seqs =
-fgrep -c ">" catalog.fa #count total number of sequences
+grep -c ">" $genome.fa #count total number of sequences
 
-grep -v "^>" catalog.fa | awk 'BEGIN {ORS=""; print ">Concatenated__catalog\n"} {print}' > catalog.concat.fa #combine into one sequence
+grep -v "^>" $genome.fa | awk 'BEGIN {ORS=""; print ">Concatenated__catalog\n"} {print}' > $genome.concat.fa #combine into one sequence
 
 echo number of seqs =
-fgrep -c ">" catalog.concat.fa #count total number of sequences - should now be 1
-head --bytes=500 catalog.concat.fa
+fgrep -c ">" $genome.concat.fa #count total number of sequences - should now be 1
+head --bytes=500 $genome.concat.fa
 
-perl -e '$count=0; $len=0; while(<>) {s/\r?\n//; s/\t/ /g; if (s/^>//) { if ($. != 1) {print "\n"} s/ |$/\t/; $count++; $_ .= "\t";} else {s/ //g; $len += length($_)} print $_;} print "\n"; warn "\nConverted $count FASTA records in $. lines to tabular format\nTotal sequence length: $len\n\n";' catalog.concat.fa | cut -f1,3 > ./fasta2tab #change format to tab
+perl -e '$count=0; $len=0; while(<>) {s/\r?\n//; s/\t/ /g; if (s/^>//) { if ($. != 1) {print "\n"} s/ |$/\t/; $count++; $_ .= "\t";} else {s/ //g; $len += length($_)} print $_;} print "\n"; warn "\nConverted $count FASTA records in $. lines to tabular format\nTotal sequence length: $len\n\n";' $genome.concat.fa | cut -f1,3 > ./$genome.fasta2tab #change format to tab
 
-head --bytes=500 fasta2tab
-tail --bytes=500 fasta2tab
+head --bytes=500 $genome.fasta2tab
+tail --bytes=500 $genome.fasta2tab
 
-perl -e '$col = 1;' -e 'while (<>) { s/\r?\n//; @F = split /\t/, $_; $len = length($F[$col]); print "$_\t$len\n" } warn "\nAdded column with length of column $col for $. lines.\n\n";' fasta2tab > ./tab_1 #add extra column per line with number of base pairs for that read
-head --bytes=500 tab_1
-tail --bytes=500 tab_1
+perl -e '$col = 1;' -e 'while (<>) { s/\r?\n//; @F = split /\t/, $_; $len = length($F[$col]); print "$_\t$len\n" } warn "\nAdded column with length of column $col for $. lines.\n\n";' $genome.fasta2tab > ./$genome.tab_1 #add extra column per line with number of base pairs for that read
+head --bytes=500 $genome.tab_1
+tail --bytes=500 $genome.tab_1
 
-awk '{print $2}' tab_1 > tab_2 #take the sequence column from tab_1 and put it into a new file, tab_2
+awk '{print $2}' $genome.tab_1 > $genome.tab_2 #take the sequence column from tab_1 and put it into a new file, tab_2
 
 #do the CG, C and G counts
-echo "CG" | awk -F\[Cc][Gg] '{print NF-1}' tab_2 > CG
-echo "C" | awk -F\[Cc] '{print NF-1}' tab_2 > C
-echo "G" | awk -F\[Gg] '{print NF-1}' tab_2 > G
+echo "CG" | awk -F\[Cc][Gg] '{print NF-1}' $genome.tab_2 > $genome.CG
+echo "C" | awk -F\[Cc] '{print NF-1}' $genome.tab_2 > $genome.C
+echo "G" | awk -F\[Gg] '{print NF-1}' $genome.tab_2 > $genome.G
 
-paste tab_1 \
-       CG \
-       C \
-       G \
-       > comb #combine into one file into the correct column
-head --bytes=500 comb
-tail --bytes=500 comb
+paste $genome.tab_1 \
+       $genome.CG \
+       $genome.C \
+       $genomeG \
+       > $genome.comb #combine into one file into the correct column
+head --bytes=500 $genome.comb
+tail --bytes=500 $genome.comb
 
-awk '{print $1, "\t", (($4)/($5*$6))*(($3^2)/($3-1))}' comb > ID_CpG #do the maths
-head --bytes=500 ID_CpG
+awk '{print $1, "\t", (($4)/($5*$6))*(($3^2)/($3-1))}' $genome.comb > $genome.ID_CpG #do the maths
+head --bytes=500 $genome.ID_CpG
