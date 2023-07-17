50b01bd18bd7a1b6b0a6feb1e3c8d90afc02664d
tillysscott <125389776+tillysscott@users.noreply.github.com>
2023-07-17 14:47:59 +0100

    Update ManualCpG.sh

--- ManualCpG.sh
+++ ManualCpG.sh
@@ -5,12 +5,12 @@
 #SBATCH --mem 20G
 
 
-head -2 catalog.fa
+head -2 genome.fa
 echo
 echo number of seqs =
-fgrep -c ">" catalog.fa #count total number of sequences
+fgrep -c ">" genome.fa #count total number of sequences
 
-perl -e '$count=0; $len=0; while(<>) {s/\r?\n//; s/\t/ /g; if (s/^>//) { if ($. != 1) {print "\n"} s/ |$/\t/; $count++; $_ .= "\t";} else {s/ //g; $len += length($_)} print $_;} print "\n"; warn "\nConverted $count FASTA records in $. lines to tabular format\nTotal sequence length: $len\n\n";' catalog.fa | cut -f1,3 > ./fasta2tab #change format to tab
+perl -e '$count=0; $len=0; while(<>) {s/\r?\n//; s/\t/ /g; if (s/^>//) { if ($. != 1) {print "\n"} s/ |$/\t/; $count++; $_ .= "\t";} else {s/ //g; $len += length($_)} print $_;} print "\n"; warn "\nConverted $count FASTA records in $. lines to tabular format\nTotal sequence length: $len\n\n";' genome.fa | cut -f1,3 > ./fasta2tab #change format to tab
 
 head -2 fasta2tab
 
